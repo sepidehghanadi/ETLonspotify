52852ada6844cbc38e577e6b6f0fdcb616b8ab3c
sepidehghanadi <dawnghanadi@gmail.com>
2022-07-16 15:04:14 +0430

    first

---
+++ spotifyetl.py
@@ -0,0 +1,115 @@
+import sqlalchemy
+import pandas as pd 
+from sqlalchemy.orm import sessionmaker
+import requests
+import json
+from datetime import datetime
+import datetime
+import sqlite3
+
+
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+USER_ID = "" # Spotify username 
+TOKEN = "" # Spotify API token
+
+#token:  https://developer.spotify.com/console/get-recently-played/
+
+def data_valid(df: pd.DataFrame) -> bool:
+    # emptiness
+    if df.empty:
+        print("No songs downloaded. Finishing execution")
+        return False 
+
+    # Primary Key
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception("Primary Key check is violated")
+
+    # nulls
+    if df.isnull().values.any():
+        raise Exception("Null values found")
+
+    # yesterday's date
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+            raise Exception("At least one of the returned songs does not have a yesterday's timestamp")
+
+    return True
+
+if __name__ == "__main__":
+
+    # Extraction
+ 
+    headers = {
+        "Accept" : "application/json",
+        "Content-Type" : "application/json",
+        "Authorization" : "Bearer {token}".format(token=TOKEN)
+    }
+    
+    # time to Unix timestamp in miliseconds      
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+   
+    r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
+
+    data = r.json()
+
+    song_names = []
+    artist_names = []
+    played_at_list = []
+    timestamps = []
+
+    # Extracting useful parts of the data from the json object      
+    for song in data["items"]:
+        song_names.append(song["track"]["name"])
+        artist_names.append(song["track"]["album"]["artists"][0]["name"])
+        played_at_list.append(song["played_at"])
+        timestamps.append(song["played_at"][0:10])
+        
+    # pandas dataframe from a dictionary    
+    song_dict = {
+        "song_name" : song_names,
+        "artist_name": artist_names,
+        "played_at" : played_at_list,
+        "timestamp" : timestamps
+    }
+
+    song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
+    
+    # Validate
+    if data_valid(song_df):
+        print("Data valid, proceed to Load stage")
+
+    # Loading
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor.execute(sql_query)
+    print("Opened database successfully")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Data already exists in the database")
+
+    conn.close()
+    print("Close database successfully")
+    
